a2083dc8c9633f1a51f48823c5fdee250fc6dd13
haoke <915775875@qq.com>
2018-09-27 15:43:50 +0800

    Initial Commit

---
+++ MHDynamicTableHeaderPrj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHDynamicTableHeaderPrj
+//
+//  Created by 孟弘 on 2018/9/27.
+//  Copyright © 2018年 孟弘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHDynamicTableHeaderPrj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHDynamicTableHeaderPrj
+//
+//  Created by 孟弘 on 2018/9/27.
+//  Copyright © 2018年 孟弘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
